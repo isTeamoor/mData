f664801f7dc4d08d93040853b877366ea0cb5367
isTeamoor <is.teamoor@gmail.com>
2023-10-13 12:55:10 +0500

    Second

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import workOrders, sCurve, reports
+
+
+app = FastAPI()
+app.include_router(workOrders.router)
+app.include_router(sCurve.router)
+app.include_router(reports.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def show_WO():
+    return {'data':'Hi'}
